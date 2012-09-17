e1679a3f9ee7bcfa0311ed8631142563cd0f7a34
Philip <philm@manjarolinux.org>
2012-09-17 14:17:13 +0200

    small changes

--- linux35/linux35/higher-rel.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for pkg in `ls | grep linux | grep -` ; do
-echo "Edit ${pkg}"
-pushd ${pkg} &>/dev/null
-_rel=$(cat PKGBUILD | grep pkgrel= | cut -d= -f2)
-sed -i -e "s/pkgrel=${_rel}/pkgrel=$((${_rel}+1))/" PKGBUILD
-popd &>/dev/null
