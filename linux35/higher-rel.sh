5f7a93e70d868309162acb041dcc46a061609f12
Philip <philm@manjarolinux.org>
2012-10-04 09:10:43 +0200

    linux35-extramodules: rebuild against 3.5.5

--- linux35/higher-rel.sh
+++ linux35/higher-rel.sh
@@ -3,7 +3,7 @@
 for pkg in $(ls | grep linux | grep -) ; do
    echo "Edit ${pkg}"
    pushd ${pkg} &>/dev/null
-   _rel=$(cat PKGBUILD | grep pkgrel= | cut -d= -f2)
+   _rel=$(cat PKGBUILD | grep pkgrel= | cut -d= -f2 | head -n1)
    sed -i -e "s/pkgrel=${_rel}/pkgrel=$((${_rel}+1))/" PKGBUILD
    popd &>/dev/null
 done
