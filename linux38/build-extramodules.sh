bfa9575cc230e500e09aa2d8a5b77fab0f165f0a
Philip <philm@manjaro.org>
2013-01-07 22:07:41 +0100

    linux38: bump to 3.8-rc2 linux38-extramodules: not all kmods build

---
+++ linux38/build-extramodules.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+pwd=`pwd`
+
+if [ "`sudo cat /etc/sudoers | grep pacman`" == "" ] ; then
+   echo "please add '`whoami` ALL=NOPASSWD: /usr/bin/pacman' to your /etc/sudoers file"
+   exit 1
+fi
+
+echo 'cleaning environment'
+rm -R ${pwd}/*/{src,pkg} -f
+echo 'building extramodules'
+cd ${pwd}/*bbswitch && makepkg --sign -s --noconfirm
+cd ${pwd}/*broadcom-wl && makepkg --sign -s --noconfirm
+cd ${pwd}/*catalyst && makepkg --sign -s --noconfirm
+#cd ${pwd}/*catalyst-legacy && makepkg --sign -d --noconfirm
+cd ${pwd}/*cdfs && makepkg --sign -s --noconfirm
+cd ${pwd}/*fcpci && makepkg --sign -s --noconfirm
+cd ${pwd}/*fcpcmcia && makepkg --sign -s --noconfirm
+cd ${pwd}/*lirc && makepkg --sign -s --noconfirm
+cd ${pwd}/*ndiswrapper && makepkg --sign -s --noconfirm
+cd ${pwd}/*nvidia && makepkg --sign -s --noconfirm
+cd ${pwd}/*nvidia-legacy && makepkg --sign -d --noconfirm
+cd ${pwd}/*nvidiabl && makepkg --sign -s --noconfirm
+cd ${pwd}/*open-vm-tools-modules && makepkg --sign -s --noconfirm
+cd ${pwd}/*r8168 && makepkg --sign -s --noconfirm
+cd ${pwd}/*rt3562sta && makepkg --sign -s --noconfirm
+cd ${pwd}/*vhba-module && makepkg --sign -s --noconfirm
+cd ${pwd}/*virtualbox-modules && makepkg --sign -s --noconfirm
+echo 'create repo'
+mkdir -p ${pwd}/repo-`uname -m`
+mv ${pwd}/*/*`uname -m`.pkg* ${pwd}/repo-`uname -m`
+ls ${pwd}/repo-`uname -m`
+echo 'building extramodules done'
---
+++ linux38/higher-rel.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for pkg in $(ls | grep linux | grep -) ; do
+   echo "Edit ${pkg}"
+   pushd ${pkg} &>/dev/null
+   _rel=$(cat PKGBUILD | grep pkgrel= | cut -d= -f2 | head -n1)
+   sed -i -e "s/pkgrel=${_rel}/pkgrel=$((${_rel}+1))/" PKGBUILD
+   popd &>/dev/null
+done
