fcbaa6e6fa52909e20ab80636b330de9677b4b84
Philip <philm@manjaro.org>
2013-01-11 20:09:29 +0100

    update linux34-extramodules update systemd

--- linux38/build-extramodules.sh
+++ linux38/build-extramodules.sh
@@ -10,23 +10,23 @@ fi
 echo 'cleaning environment'
 rm -R ${pwd}/*/{src,pkg} -f
 echo 'building extramodules'
-cd ${pwd}/*bbswitch && makepkg --sign -s --noconfirm
-cd ${pwd}/*broadcom-wl && makepkg --sign -s --noconfirm
-cd ${pwd}/*catalyst && makepkg --sign -s --noconfirm
+cd ${pwd}/*bbswitch && makepkg --sign -sf --noconfirm
+cd ${pwd}/*broadcom-wl && makepkg --sign -sf --noconfirm
+cd ${pwd}/*catalyst && makepkg --sign -sf --noconfirm
 #cd ${pwd}/*catalyst-legacy && makepkg --sign -d --noconfirm
-cd ${pwd}/*cdfs && makepkg --sign -s --noconfirm
-cd ${pwd}/*fcpci && makepkg --sign -s --noconfirm
-cd ${pwd}/*fcpcmcia && makepkg --sign -s --noconfirm
-cd ${pwd}/*lirc && makepkg --sign -s --noconfirm
-cd ${pwd}/*ndiswrapper && makepkg --sign -s --noconfirm
-cd ${pwd}/*nvidia && makepkg --sign -s --noconfirm
-cd ${pwd}/*nvidia-legacy && makepkg --sign -d --noconfirm
-cd ${pwd}/*nvidiabl && makepkg --sign -s --noconfirm
-cd ${pwd}/*open-vm-tools-modules && makepkg --sign -s --noconfirm
-cd ${pwd}/*r8168 && makepkg --sign -s --noconfirm
-cd ${pwd}/*rt3562sta && makepkg --sign -s --noconfirm
-cd ${pwd}/*vhba-module && makepkg --sign -s --noconfirm
-cd ${pwd}/*virtualbox-modules && makepkg --sign -s --noconfirm
+cd ${pwd}/*cdfs && makepkg --sign -sf --noconfirm
+cd ${pwd}/*fcpci && makepkg --sign -sf --noconfirm
+cd ${pwd}/*fcpcmcia && makepkg --sign -sf --noconfirm
+cd ${pwd}/*lirc && makepkg --sign -sf --noconfirm
+cd ${pwd}/*ndiswrapper && makepkg --sign -sf --noconfirm
+cd ${pwd}/*nvidia && makepkg --sign -sf --noconfirm
+cd ${pwd}/*nvidia-legacy && makepkg --sign -df --noconfirm
+cd ${pwd}/*nvidiabl && makepkg --sign -sf --noconfirm
+cd ${pwd}/*open-vm-tools-modules && makepkg --sign -sf --noconfirm
+cd ${pwd}/*r8168 && makepkg --sign -sf --noconfirm
+cd ${pwd}/*rt3562sta && makepkg --sign -sf --noconfirm
+cd ${pwd}/*vhba-module && makepkg --sign -sf --noconfirm
+cd ${pwd}/*virtualbox-modules && makepkg --sign -sf --noconfirm
 echo 'create repo'
 mkdir -p ${pwd}/repo-`uname -m`
 mv ${pwd}/*/*`uname -m`.pkg* ${pwd}/repo-`uname -m`
