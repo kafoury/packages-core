02f3ba5380a0fafd635ca711f22e488610ca8ea5
Philip <philm@manjaro.org>
2013-02-21 17:05:23 +0100

    linux38-extramodules: small changes

--- linux38/build-extramodules.sh
+++ linux38/build-extramodules.sh
@@ -13,18 +13,22 @@ echo 'building extramodules'
 cd ${pwd}/*bbswitch && makepkg --sign -sf --noconfirm
 cd ${pwd}/*broadcom-wl && makepkg --sign -sf --noconfirm
 cd ${pwd}/*catalyst && makepkg --sign -sf --noconfirm
-#cd ${pwd}/*catalyst-legacy && makepkg --sign -d --noconfirm
+#cd ${pwd}/*catalyst-legacy && makepkg --sign -df --noconfirm
 cd ${pwd}/*cdfs && makepkg --sign -sf --noconfirm
 cd ${pwd}/*fcpci && makepkg --sign -sf --noconfirm
 cd ${pwd}/*fcpcmcia && makepkg --sign -sf --noconfirm
 cd ${pwd}/*lirc && makepkg --sign -sf --noconfirm
 cd ${pwd}/*ndiswrapper && makepkg --sign -sf --noconfirm
 cd ${pwd}/*nvidia && makepkg --sign -sf --noconfirm
-cd ${pwd}/*nvidia-legacy && makepkg --sign -df --noconfirm
+cd ${pwd}/*nvidia-legacy && makepkg --sign -d --noconfirm
 cd ${pwd}/*nvidiabl && makepkg --sign -sf --noconfirm
 cd ${pwd}/*open-vm-tools-modules && makepkg --sign -sf --noconfirm
 cd ${pwd}/*r8168 && makepkg --sign -sf --noconfirm
 cd ${pwd}/*rt3562sta && makepkg --sign -sf --noconfirm
+if [ "$(uname -m)" == 'i686' ] ; then
+   cd ${pwd}/*slmodem && makepkg --sign -sf --noconfirm
+fi
+cd ${pwd}/*tp_smapi && makepkg --sign -sf --noconfirm
 cd ${pwd}/*vhba-module && makepkg --sign -sf --noconfirm
 cd ${pwd}/*virtualbox-modules && makepkg --sign -sf --noconfirm
 echo 'create repo'
