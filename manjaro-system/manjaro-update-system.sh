bb27c493f2c1f1ea251b015b5da85f0f8ea15a92
Roland Singer <roland@manjaro.org>
2013-06-07 16:27:55 +0200

    updated manjaro-system

--- manjaro-system/manjaro-update-system.sh
+++ manjaro-system/manjaro-update-system.sh
@@ -41,36 +41,68 @@ post_upgrade() {
 			local path="/var/lib/pacman/local/$(pacman -Q ${pkg} | sed 's/ /-/g')/files"
 			if [ ! -f "${path}" ]; then continue; fi
 
-			sed -i -e 's|^bin/|usr/bin/|' -e 's|^sbin/|usr/bin/|' -e 's|^usr/sbin/|usr/bin/|' "${path}"
+			sed -i -e 's|^bin/|usr/bin/|' -e 's|^sbin/|usr/bin/|' -e 's|^usr/sbin/|usr/bin/|' -e 's|^bin$|usr/bin|' -e 's|^sbin$|usr/bin|' -e 's|^usr/sbin$|usr/bin|' "${path}"
 		done
 
 		# Move files
-# TODO: check symlinks valid!
 		for file in $files
 		do
 			local filename="${file##*/}"
 			if [ "${filename}" == "" ]; then continue; fi
 
-			if [ -e "/usr/bin/${filename}" ]; then
-				err "'/usr/bin/${filename}' already exists! Moving file to '/usr/bin_duplicates/${filename}'"
-				if [ ! -d "/usr/bin_duplicates" ]; then mkdir "/usr/bin_duplicates"; fi
-				mv "${file}" "/usr/bin_duplicates/"
+			# Check if file is a symlink
+			if [ -L "${file}" ]; then
+				local filelink="$(readlink "${file}")"
+
+				# Check if destination file exists. Otherwise move the symlink.
+				if [ -e "/usr/bin/${filename}" ]; then
+					# Remove link
+					unlink "${file}"
+				elif [[ "${file}" =~ ^"/bin/".*|^"/sbin/".* ]] && [[ "${filelink}" =~ ^"../usr/bin/".* ]]; then
+					# Move link to /usr/bin and update relative path
+					filelink="$(echo "${filelink}" | sed 's|^\.\./usr/bin/||')"
+					ln -s "${filelink}" "/usr/bin/${filename}"
+					unlink "${file}"
+				elif [[ "${file}" =~ ^"/bin/".*|^"/sbin/".* ]] && [[ "${filelink}" =~ ^"../".* ]]; then
+					# Move link to /usr/bin and update relative path
+					filelink="$(echo "${filelink}" | sed 's|^\.\./|\.\./\.\./|')"
+					ln -s "${filelink}" "/usr/bin/${filename}"
+					unlink "${file}"
+				elif [[ "${file}" =~ ^"/usr/sbin/".* ]] && [[ "${filelink}" =~ ^"../bin/".* ]]; then
+					# Move link to /usr/bin and update relative path
+					filelink="$(echo "${filelink}" | sed 's|^\.\./bin/||')"
+					ln -s "${filelink}" "/usr/bin/${filename}"
+					unlink "${file}"
+				else
+					# Move link as it is
+					ln -s "${filelink}" "/usr/bin/${filename}"
+					unlink "${file}"
+				fi
 			else
-				mv "${file}" "/usr/bin/"
+				if [ -L "/usr/bin/${filename}" ]; then
+					unlink "/usr/bin/${filename}"
+					mv "${file}" "/usr/bin/"
+				elif [ -e "/usr/bin/${filename}" ]; then
+					err "'/usr/bin/${filename}' already exists! Moving file to '/usr/bin_duplicates/${filename}'"
+					if [ ! -d "/usr/bin_duplicates" ]; then mkdir "/usr/bin_duplicates"; fi
+					mv "${file}" "/usr/bin_duplicates/"
+				else
+					mv "${file}" "/usr/bin/"
+				fi
 			fi
 		done
+
+		# filesystem should own /bin, /sbin and /usr/sbin
+		local filesystem_path="/var/lib/pacman/local/$(pacman -Q filesystem | sed 's/ /-/g')/files"
+		sed -i 's|^\%FILES\%|\%FILES\%\nbin\nsbin\nusr/sbin|' "${filesystem_path}"
 
 		# Remove directories and create symlinks
 		rm -fr /bin
 		rm -fr /sbin
 		rm -fr /usr/sbin
-		ln -s /usr/bin /bin
-		ln -s /usr/bin /sbin
-		ln -s /usr/bin /usr/sbin
-
-		# Update filesystem
-		rm /var/lib/pacman/db.lck
-		pacman --noconfirm --force -S filesystem
+		ln -s usr/bin /bin
+		ln -s usr/bin /sbin
+		ln -s bin /usr/sbin
 
 		msg "Run pacman -Syu again!"
 	fi
