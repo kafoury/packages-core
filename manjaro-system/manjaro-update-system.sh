d7268129926045f1dbc1ce1c0318bcb76b56b9cb
Philip <philm@manjaro.org>
2014-02-12 20:16:48 +0100

    Updates - bump kernels - update locale-info to 2.19 - add Rob McCathie to manjaro-keyring - update mhwd for Catalyst 13.35 - bump release to 0.8.9 - add new mirrors to mirrorlist

--- manjaro-system/manjaro-update-system.sh
+++ manjaro-system/manjaro-update-system.sh
@@ -43,6 +43,21 @@ detectDE()
 
 post_upgrade() {
 
+	# workaround for catalyst-server removal
+	pacman -Qq catalyst-server &> /tmp/cmd1
+	pacman -Q catalyst-utils &> /tmp/cmd2
+	packages="catalyst-server catalyst-input catalyst-video"
+	conflicts="xf86-input-acecad xf86-input-aiptek xf86-input-evdev xf86-input-joystick xf86-input-keyboard xf86-input-mouse xf86-input-synaptics xf86-input-void xf86-input-wacom xorg-server-common xorg-server"
+	if [ "$(grep 'catalyst-server' /tmp/cmd1)" == "catalyst-server" ]; then
+	   if [ "$(cat /tmp/cmd2 | cut -d- -f2 | cut -d" " -f2 | sed -e 's/\.//g')" -lt "13351005" ]; then
+	      msg "Preparing Catalyst installation ..."
+	      rm /var/lib/pacman/db.lck &> /dev/null
+	      check_pkgs
+	      pacman --noconfirm -Rdd ${packages} &> /dev/null
+	      pacman --noconfirm -S ${conflicts} &> /dev/null
+	   fi
+	fi
+
 	# remove pyc-files if python-dbus < 1.2.0-2
 	pacman -Qq python-dbus &> /tmp/cmd1
 	pacman -Q python-dbus &> /tmp/cmd2
